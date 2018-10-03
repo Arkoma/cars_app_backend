ab1adfd8caa331e8b74053c466c03fef524032bc
Daniel Akintitan <daniel.akintitan@cognizant.com>
2018-10-03 15:33:19 -0500

    added CrossOrigin

--- server/src/main/java/com/arkoma/demo/controller/CoolCarController.java
+++ server/src/main/java/com/arkoma/demo/controller/CoolCarController.java
@@ -2,6 +2,7 @@ package com.arkoma.demo.controller;
 
 import com.arkoma.demo.entities.Car;
 import com.arkoma.demo.repository.CarRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class CoolCarController {
     }
 
     @GetMapping("/cool-cars")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Car> coolCars() {
         return carRepository.findAll().stream()
                 .filter(this::isCool)
--- server/src/main/java/com/arkoma/demo/repository/CarRepository.java
+++ server/src/main/java/com/arkoma/demo/repository/CarRepository.java
@@ -3,8 +3,10 @@ package com.arkoma.demo.repository;
 import com.arkoma.demo.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "https://localhost:4200")
 public interface CarRepository extends JpaRepository<Car, Long> {
 
 }
