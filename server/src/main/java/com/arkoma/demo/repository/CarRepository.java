b044ab6aa78abd4a29539f35411804ddd9bc9bc9
Daniel Akintitan <daniel.akintitan@cognizant.com>
2018-10-03 14:22:37 -0500

    finish basic back end

--- server/src/main/java/com/arkoma/demo/repository/CarRepository.java
+++ server/src/main/java/com/arkoma/demo/repository/CarRepository.java
@@ -1,4 +1,10 @@
 package com.arkoma.demo.repository;
 
-public interface CarRepository {
+import com.arkoma.demo.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CarRepository extends JpaRepository<Car, Long> {
+
 }
